bc669159dbc6a53bc0e9bad8f9334dd853bdf9eb
Hiroaki Nakamura <hnakamur@gmail.com>
2017-06-14 23:15:14 +0900

    Use webapputil.HTTPError and an error handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,10 +18,9 @@ import (
 	"github.com/hnakamur/ltsvlog"
 )
 
-func handleGetList(w http.ResponseWriter, r *http.Request) {
+func handleGetList(w http.ResponseWriter, r *http.Request) *webapputil.HTTPError {
 	ltsvlog.Logger.Info().String("handler", "handleGetList").String("reqID", webapputil.RequestID(r)).Log()
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprintln(w, "handleGetList -------- Standard Library ---------")
 	fmt.Fprintln(w, "Method:", r.Method)
 	fmt.Fprintln(w, "URL:", r.URL.String())
@@ -28,10 +28,32 @@ func handleGetList(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "HTTPS:", os.Getenv("HTTPS"))
 	name := "Golang GCI"
 	fmt.Fprintf(w, "hello:%s!!", name)
+	return nil
 }
 
-func handleGetRoot(w http.ResponseWriter, r *http.Request) {
+func handleGetRoot(w http.ResponseWriter, r *http.Request) *webapputil.HTTPError {
 	ltsvlog.Logger.Info().String("handler", "handleGetRoot").String("reqID", webapputil.RequestID(r)).Log()
+	err := r.ParseForm()
+	if err != nil {
+		err = ltsvlog.WrapErr(err, func(err error) error {
+			return fmt.Errorf("failed to parse form, err=%s", err)
+		})
+		return webapputil.NewHTTPError(err, http.StatusBadRequest)
+	}
+	statusStr := r.FormValue("status")
+	if statusStr != "" {
+		status, err := strconv.Atoi(statusStr)
+		if err != nil {
+			err = ltsvlog.WrapErr(err, func(err error) error {
+				return fmt.Errorf("failed to convert status parameter to int, err=%s", err)
+			}).String("statusStr", statusStr)
+			return webapputil.NewHTTPError(err, http.StatusBadRequest)
+		}
+
+		err = ltsvlog.Err(errors.New("intentional error for testing"))
+		return webapputil.NewHTTPError(err, status)
+	}
+
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprintln(w, "handleGetRoot -------- Standard Library ---------")
 	fmt.Fprintln(w, "Method:", r.Method)
@@ -40,6 +62,7 @@ func handleGetRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "HTTPS:", os.Getenv("HTTPS"))
 	name := "Golang GCI"
 	fmt.Fprintf(w, "hello:%s!!", name)
+	return nil
 }
 
 func main() {
@@ -82,9 +105,23 @@ func main() {
 
 	scriptName := os.Getenv("SCRIPT_NAME")
 
+	errorHandler := func(err *webapputil.HTTPError, w http.ResponseWriter, r *http.Request) {
+		lerr, ok := err.Error.(*ltsvlog.Error)
+		if !ok {
+			lerr = ltsvlog.Err(err.Error)
+		}
+		ltsvlog.Logger.Err(lerr.Int("status", err.Status).
+			String("reqID", webapputil.RequestID(r)))
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(err.Status)
+	}
+	withErrorHandler := func(next func(w http.ResponseWriter, r *http.Request) *webapputil.HTTPError) http.Handler {
+		return webapputil.WithErrorHandler(next, errorHandler)
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc(scriptName+"/list", handleGetList)
-	mux.HandleFunc(scriptName, handleGetRoot)
+	mux.Handle(scriptName+"/list", withErrorHandler(handleGetList))
+	mux.Handle(scriptName, withErrorHandler(handleGetRoot))
 	err = cgi.Serve(webapputil.RequestIDMiddleware(
 		webapputil.AccessLogMiddleware(mux, writeAccessLog),
 		generateRequestID))
